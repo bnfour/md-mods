0802743e8171978d47451d1a00d47d0c518fe6a9
Bn4 <bnfour@bnfour.net>
2023-02-27 18:35:01 +0700

    true rank 1k+ mod: actually implemented seems to work ¯\_(ツ)_/¯

---
+++ TrueAbove1kRank/Patches/PnlRankUIRefreshPatch.cs
@@ -0,0 +1,24 @@
+using System;
+
+using HarmonyLib;
+using Newtonsoft.Json;
+
+using Assets.Scripts.UI.Panels;
+
+namespace Bnfour.MuseDashMods.TrueAbove1kRank.Patches
+{
+    [HarmonyPatch(typeof(PnlRank), "UIRefresh")]
+    public class PnlRankUIRefreshPatch
+    {
+        private static void Postfix(string uid, PnlRank __instance)
+        {
+            if (__instance.m_SelfRank.ContainsKey(uid)
+                && __instance.txtServerRank.IsActive()
+                && __instance.txtServerRank.text.Contains("+"))
+            {
+                var selfRank = JsonConvert.DeserializeObject<Data.Api.SelfRank>(__instance.m_SelfRank[uid].ToString());
+                __instance.txtServerRank.text = selfRank.DisplayRank.ToString();
+            }
+        }
+    }
+}
