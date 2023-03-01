49518e929f7494a26ad8315bc08bcb398eb2be7e
Bn4 <bnfour@bnfour.net>
2023-03-02 04:23:28 +0700

    true rank mod: changed _display_ name to "true rank for 999+" for the great justice too lazy to rename it elsewhere

--- TrueAbove1kRank/Properties/AssemblyInfo.cs
+++ TrueAbove1kRank/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using MelonLoader;
 using Bnfour.MuseDashMods.TrueAbove1kRank;
 
 // TODO github repo link?
-[assembly: MelonInfo(typeof(TrueAbove1kRankMod), "True rank for 1k+", "0.1.0", "bnfour")]
+[assembly: MelonInfo(typeof(TrueAbove1kRankMod), "True rank for 999+", "0.1.0", "bnfour")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(ConsoleColor.Cyan)]
 [assembly: MelonAuthorColor(ConsoleColor.Gray)]
