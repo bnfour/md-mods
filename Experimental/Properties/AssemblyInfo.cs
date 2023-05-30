e8379fe1e58f65ccd681219a8cf55f8bf1a9f5e5
Bn4 <bnfour@bnfour.net>
2023-05-30 21:28:39 +0700

    implemented an experimental mod to do experimental things
    It's a place to do various things without modifying existing mods. The project's code in this repo will probably be a helloworld forever, but any new mod ideas will be tested there locally.

---
+++ Experimental/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using MelonLoader;
+
+using Bnfour.MuseDashMods.Experimental;
+
+[assembly: MelonInfo(typeof(ExperimentalMod), "Mystery in-development mod", "0.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+[assembly: MelonColor(ConsoleColor.Cyan)]
+[assembly: MelonAuthorColor(ConsoleColor.Gray)]
+
+[assembly: AssemblyDescription("Playground for experimenting. Not a real mod.")]
+[assembly: AssemblyCopyright("bnfour 2023; open-source")]
