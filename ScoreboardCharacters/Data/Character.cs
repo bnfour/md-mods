ef6b13138d787f13a1e8c274ed1cd0e9c1818f2c
Bn4 <bnfour@bnfour.net>
2023-10-08 17:06:11 +0700

    scoreboard mod: added more mystery char/elfin ids for the entire spritesheet

--- ScoreboardCharacters/Data/Character.cs
+++ ScoreboardCharacters/Data/Character.cs
@@ -28,12 +28,21 @@ namespace Bnfour.MuseDashMods.ScoreboardCharacters.Data
         OlaBoxer,
         BuroExorcist,
         HatsuneMiku,
-        // these are here so that we can use the IDs even without an update
+        // these are here so that we can use typed IDs even without an update
         MysteryCharacter26,
         MysteryCharacter27,
         MysteryCharacter28,
         MysteryCharacter29,
-        MysteryCharacter30
-        // to be continued?
+        MysteryCharacter30,
+        MysteryCharacter31,
+        MysteryCharacter32,
+        MysteryCharacter33,
+        MysteryCharacter34,
+        MysteryCharacter35,
+        MysteryCharacter36,
+        MysteryCharacter37,
+        MysteryCharacter38,
+        MysteryCharacter39
+        // if you've reached here, it's time to extend the spritesheet
     }
 }
