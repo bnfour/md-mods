dabd08414bd2bef79649cc8a9a1403e44df276ad
Bn4 <bnfour@bnfour.net>
2023-11-17 15:48:52 +0700

    no way!? created a project for a new mod

---
+++ AlbumScroll/AlbumScrollMod.cs
@@ -0,0 +1,14 @@
+using MelonLoader;
+
+namespace Bnfour.MuseDashMods.AlbumScroll
+{
+    /// <summary>
+    /// A mod that allows to scroll to the next album in the song selection screen
+    /// by holding shift while scrolling.
+    /// TODO This class manages key state for the hooks.
+    /// </summary>
+    public class AlbumScrollMod : MelonMod
+    {
+        
+    }
+}
