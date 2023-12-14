154dd5f1606e5618aa714f0a231995f73e692ae1
Bn4 <bnfour@bnfour.net>
2023-12-14 20:53:02 +0700

    socreboard mod -- rescaling: probably completed the split of ButtonImageProvider to image load and sprite creation parts

--- ScoreboardCharacters/Utilities/ButtonImages/ButtonImageProvider.cs
+++ ScoreboardCharacters/Utilities/ButtonImages/ButtonImageProvider.cs
@@ -1,12 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
+
+using Graphics = System.Drawing.Graphics;
 
 using Bnfour.MuseDashMods.ScoreboardCharacters.Data;
 
@@ -14,201 +12,64 @@ namespace Bnfour.MuseDashMods.ScoreboardCharacters.Utilities.ButtonImages
 {
     /// <summary>
     /// Generates images for the custom buttons from the built-in spritesheet or file-based override.
-    /// Has an internal image cache that lasts until playing a level.
+    /// Has an internal image cache that lasts until playing a level (or resolution change).
     /// </summary>
     public class ButtonImageProvider
     {
-        // please note that this class only cares about vertical resolution,
-        // so any mention of "resolution" there is a single number of screen height
+        private readonly Dictionary<(Character, Elfin), Sprite> _cache = new Dictionary<(Character, Elfin), Sprite>();
 
-        // TODO move constants and some fields to other classes
+        private readonly SpritesheetManager _manager = new SpritesheetManager();
 
-        private const string EmbeddedResourcePrescaledNameTemplate = "Bnfour.MuseDashMods.ScoreboardCharacters.Resources.sprites.{0}.png";
-
-        private const string OverrideFilename = "scoreboard_characters_override.png";
-
-        private readonly int[] SupportedResolutions = { 720, 1080, 1440, 2160 };
-
-        // using 1920×1080 as baseline resolution
-        private const int BaseSpriteSize = 40;
-        private const int BaseResolution = 1080;
-
-        private readonly string EmbeddedResourceFallbackName;
-
-        private Bitmap CustomAtlas;
-
-        private readonly Dictionary<(Character, Elfin), Sprite> Cache = new Dictionary<(Character, Elfin), Sprite>();
-
-        private const int CharactersPerRow = 5;
-        private const int ElfinsPerRow = 3;
-
-        private const int SpritesPerRow = CharactersPerRow + ElfinsPerRow;
-        private const int ElfinStartColumn = 5;
-
-        private int SpriteSize;
-        private Rectangle CharacterDestinationRectangle;
-        private Rectangle ElfinDestinationRectangle;
-        private int CurrentResolution;
-
-        private bool OverrideActive;
+        private SpritesheetSettings _settings;
 
         public ButtonImageProvider()
         {
-            // use highest res available as fallback for non-supported resolution
-            EmbeddedResourceFallbackName = string.Format(EmbeddedResourcePrescaledNameTemplate, SupportedResolutions.Max());
-
-            ConfigureScalingIfNeeded();
+            _settings = _manager.LoadSpritesheet();
         }
 
         public Sprite GetSprite(Character character, Elfin elfin)
         {
-            ConfigureScalingIfNeeded();
+            if (_manager.ReloadRequired())
+            {
+                _settings = _manager.LoadSpritesheet();
+                ResetCache();
+            }
 
             var keyTuple = (character, elfin);
 
-            if (Cache.ContainsKey(keyTuple))
+            if (_cache.ContainsKey(keyTuple))
             {
-                return Cache[keyTuple];
+                return _cache[keyTuple];
             }
 
             var newSprite = CreateSprite(character, elfin);
-            Cache[keyTuple] = newSprite;
+            _cache[keyTuple] = newSprite;
             return newSprite;
         }
 
         public void ResetCache()
         {
-            Cache.Clear();
-        }
-
-        private void ConfigureScalingIfNeeded()
-        {
-            // do nothing if resulotion is current or the image override is in effect
-            var screenHeight = Screen.height;
-            if (CurrentResolution == screenHeight || OverrideActive)
-            {
-                return;
-            }
-
-            CurrentResolution = screenHeight;
-
-            if (!TryLoadOverride())
-            {
-                LoadDefaultSpritesheet(CurrentResolution);
-            }
-
-            // clear the cache in case resolution was switched at runtime
-            ResetCache();
-        }
-
-        /// <summary>
-        /// Loads the default spritesheet for a given resolution.
-        /// If no pre-scaled version is available in the resources, on the fly rescale is performed.
-        /// </summary>
-        /// <param name="resolution">Target vertical resolution.</param>
-        private void LoadDefaultSpritesheet(int resolution)
-        {
-            SpriteSize = (int)Math.Round(BaseSpriteSize * (decimal)resolution / BaseResolution, MidpointRounding.ToEven);
-            CharacterDestinationRectangle = new Rectangle(0, 0, SpriteSize, SpriteSize);
-            ElfinDestinationRectangle = new Rectangle(SpriteSize, 0, SpriteSize, SpriteSize);
-
-            var isSupported = SupportedResolutions.Contains(resolution);
-            var resName = isSupported
-                ? string.Format(EmbeddedResourcePrescaledNameTemplate, resolution)
-                : EmbeddedResourceFallbackName;
-
-            var assembly = typeof(ButtonImageProvider).GetTypeInfo().Assembly;
-            var defaultImageStream = assembly.GetManifestResourceStream(resName);
-            var defaultBitmap = new Bitmap(defaultImageStream);
-
-            if (!isSupported)
-            {
-                defaultBitmap = RescaleSpritesheet(defaultBitmap, SpriteSize);
-            }
-
-            CustomAtlas = defaultBitmap;
-        }
-
-        /// <summary>
-        /// Tries to load the override image, and sets it as the sprite source
-        /// if it contains square sprites with the same size, <see cref="SpritesPerRow"/> per row.
-        /// Please note it only checks image dimensions, not the contents.
-        /// </summary>
-        /// <returns>True if the override was successfully loaded, false otherwise.</returns>
-        private bool TryLoadOverride()
-        {
-            var overrideFullPath = Path.Combine(Application.dataPath, OverrideFilename);
-            if (File.Exists(overrideFullPath))
-            {
-                var overrideBitmap = new Bitmap(overrideFullPath);
-                
-                if (overrideBitmap.Width % SpritesPerRow != 0)
-                {
-                    // TODO consider some kind of exception to indicate what exactly went wrong with override load -- 2023-12-14
-                    // there should be 8 sprites per row
-                    return false;
-                }
-                int potentialOverrideSpriteSize = overrideBitmap.Width / SpritesPerRow;
-                if (overrideBitmap.Height % potentialOverrideSpriteSize != 0)
-                {
-                    // there should be integer amount of rows
-                    return false;
-                }
-
-                SpriteSize = potentialOverrideSpriteSize;
-                CharacterDestinationRectangle = new Rectangle(0, 0, SpriteSize, SpriteSize);
-                ElfinDestinationRectangle = new Rectangle(SpriteSize, 0, SpriteSize, SpriteSize);
-
-                OverrideActive = true;
-                CustomAtlas = overrideBitmap;
-
-                return true;
-            }
-
-            return false;
-        }
-
-        private Bitmap RescaleSpritesheet(Bitmap source, int targetSpriteSize)
-        {
-            // get number of rows from builtin image (premature optimization if it will be extended downwards for more character space),
-            // assuming individual sprites are square
-            var spriteRows = source.Height / (source.Width / SpritesPerRow);
-            
-            var scaledBitmap = new Bitmap(SpritesPerRow * targetSpriteSize, spriteRows * targetSpriteSize);
-
-            var sourceRect = new Rectangle(0, 0, source.Width, source.Height);
-            var destRect = new Rectangle(0, 0, scaledBitmap.Width, scaledBitmap.Height);
-
-            using (var graphics = System.Drawing.Graphics.FromImage(scaledBitmap))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                graphics.DrawImage(source, destRect, sourceRect, GraphicsUnit.Pixel);
-
-                return scaledBitmap;
-            }
+            _cache.Clear();
         }
 
         private Sprite CreateSprite(Character character, Elfin elfin)
         {
-            var buttonBitmap = new Bitmap(2 * SpriteSize, SpriteSize);
-            using (var graphics = System.Drawing.Graphics.FromImage(buttonBitmap))
+            var size = _settings.SpriteSize;
+            var buttonBitmap = new Bitmap(2 * size, size);
+            using (var graphics = Graphics.FromImage(buttonBitmap))
             {
-                graphics.DrawImage(CustomAtlas, CharacterDestinationRectangle, GetSpriteRectangle(character), GraphicsUnit.Pixel);
-                graphics.DrawImage(CustomAtlas, ElfinDestinationRectangle, GetSpriteRectangle(elfin), GraphicsUnit.Pixel);
+                graphics.DrawImage(_settings.Bitmap, _settings.CharacterDest, GetSpriteRectangle(character), GraphicsUnit.Pixel);
+                graphics.DrawImage(_settings.Bitmap, _settings.ElfinDest, GetSpriteRectangle(elfin), GraphicsUnit.Pixel);
                 using (var byteStream = new MemoryStream())
                 {
                     buttonBitmap.Save(byteStream, ImageFormat.Png);
                     // texture size here is irrelevant as it gets changed by LoadImage,
-                    // also turning mipmapping off
+                    // mipmap is off as we supply pre-scaled images and do not want any unity scaling involved
                     var texture = new Texture2D(1, 1, TextureFormat.RGBA32, false);
                     ImageConversion.LoadImage(texture, byteStream.ToArray());
-                    // Rect is not a Rectangle, unfortunate mixing in one file
-                    var sprite = Sprite.Create(texture, new Rect(0, 0, 2 * SpriteSize, SpriteSize), new Vector2(0.5f, 0.5f));
+                    // (UnityEngine.)Rect is not a (System.Drawing.)Rectangle, unfortunate mixing in one file
+                    // just for reference, their vertical axis seem to go to different directions, it was an issue earlier (see #8)
+                    var sprite = Sprite.Create(texture, new Rect(0, 0, 2 * size, size), new Vector2(0.5f, 0.5f));
 
                     return sprite;
                 }
@@ -218,22 +79,24 @@ namespace Bnfour.MuseDashMods.ScoreboardCharacters.Utilities.ButtonImages
         private Rectangle GetSpriteRectangle(Character character)
         {
             var spriteIndex = (int)character;
+            var size = _settings.SpriteSize;
 
-            var columnIndex = spriteIndex % CharactersPerRow;
-            var rowIndex = spriteIndex / CharactersPerRow;
+            var columnIndex = spriteIndex % Constants.CharactersPerRow;
+            var rowIndex = spriteIndex / Constants.CharactersPerRow;
 
-            return new Rectangle(columnIndex * SpriteSize, rowIndex * SpriteSize, SpriteSize, SpriteSize);
+            return new Rectangle(columnIndex * size, rowIndex * size, size, size);
         }
 
         private Rectangle GetSpriteRectangle(Elfin elfin)
         {
             // elfins start from -1
             var spriteIndex = (int)elfin + 1;
+            var size = _settings.SpriteSize;
 
-            var columnIndex = spriteIndex % ElfinsPerRow;
-            var rowIndex = spriteIndex / ElfinsPerRow;
+            var columnIndex = spriteIndex % Constants.ElfinsPerRow;
+            var rowIndex = spriteIndex / Constants.ElfinsPerRow;
 
-            return new Rectangle((ElfinStartColumn + columnIndex) * SpriteSize, rowIndex * SpriteSize, SpriteSize, SpriteSize);
+            return new Rectangle((Constants.ElfinStartColumn + columnIndex) * size, rowIndex * size, size, size);
         }
     }
 }
--- ScoreboardCharacters/Utilities/ButtonImages/SpritesheetManager.cs
+++ ScoreboardCharacters/Utilities/ButtonImages/SpritesheetManager.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace Bnfour.MuseDashMods.ScoreboardCharacters.Utilities.ButtonImages
 {
@@ -7,18 +13,116 @@ namespace Bnfour.MuseDashMods.ScoreboardCharacters.Utilities.ButtonImages
     /// </summary>
     public class SpritesheetManager
     {
-        // TODO constants, "constants", spritesheet-related fields from ButtonImageProvider
+        private const string EmbeddedResourcePrescaledNameTemplate = "Bnfour.MuseDashMods.ScoreboardCharacters.Resources.sprites.{0}.png";
+        private const string OverrideFilename = "scoreboard_characters_override.png";
+        private readonly string EmbeddedResourceFallbackName;
+
+        private readonly int[] SupportedResolutions = { 720, 1080, 1440, 2160 };
+        // 1920×1080 is the baseline resolution
+        private const int BaseResolution = 1080;
+        private const int BaseSpriteSize = 40;
+
+        private int _currentResolution;
+        private bool _overrideActive;
+
+        public SpritesheetManager()
+        {
+            // use highest res available as fallback for non-supported resolution
+            EmbeddedResourceFallbackName = string.Format(EmbeddedResourcePrescaledNameTemplate, SupportedResolutions.Max());
+        }
 
         public SpritesheetSettings LoadSpritesheet()
         {
-            throw new NotImplementedException("soon™");
+            _currentResolution = Screen.height;
+            return LoadOverride() ?? LoadDefault();
         }
 
         public bool ReloadRequired()
         {
-            throw new NotImplementedException("soon™");
+            return !_overrideActive && Screen.height != _currentResolution;
         }
 
-        // TODO move LoadDefaultSpritesheet and TryLoadOverride here
+        /// <summary>
+        /// Tries to load the override image, if it contains square sprites with the same size, <see cref="Constants.SpritesPerRow"/> per row.
+        /// Please note it only checks image dimensions, not the contents.
+        /// </summary>
+        /// <returns>
+        /// Settings for the spritesheet if the override was successfully loaded,
+        /// null if the override is not loaded for any reason (it doesn't exist or the resolution is wrong).
+        /// </returns>
+        private SpritesheetSettings LoadOverride()
+        {
+            var overrideFullPath = Path.Combine(Application.dataPath, OverrideFilename);
+            if (File.Exists(overrideFullPath))
+            {
+                var overrideBitmap = new Bitmap(overrideFullPath);
+                if (overrideBitmap.Width % Constants.SpritesPerRow != 0)
+                {
+                    // TODO consider some kind of exception to indicate what exactly went wrong with override load -- 2023-12-14
+                    // there should be 8 sprites per row
+                    return null;
+                }
+                int potentialOverrideSpriteSize = overrideBitmap.Width / Constants.SpritesPerRow;
+                if (overrideBitmap.Height % potentialOverrideSpriteSize != 0)
+                {
+                    // there should be an integer amount of rows
+                    return null;
+                }
+
+                _overrideActive = true;
+                return new SpritesheetSettings(overrideBitmap, potentialOverrideSpriteSize);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Loads the default spritesheet for current resolution.
+        /// If no pre-scaled version is available in the resources, on the fly rescale is performed.
+        /// </summary>
+        private SpritesheetSettings LoadDefault()
+        {
+            int spriteSize = (int)Math.Round(BaseSpriteSize * (decimal)_currentResolution / BaseResolution, MidpointRounding.ToEven);
+
+            var isSupported = SupportedResolutions.Contains(_currentResolution);
+            var resName = isSupported
+                ? string.Format(EmbeddedResourcePrescaledNameTemplate, _currentResolution)
+                : EmbeddedResourceFallbackName;
+
+            var assembly = typeof(ButtonImageProvider).GetTypeInfo().Assembly;
+            var defaultImageStream = assembly.GetManifestResourceStream(resName);
+            var defaultBitmap = new Bitmap(defaultImageStream);
+            if (!isSupported)
+            {
+                defaultBitmap = RescaleSpritesheet(defaultBitmap, spriteSize);
+            }
+
+            return new SpritesheetSettings(defaultBitmap, spriteSize);
+        }
+
+        private Bitmap RescaleSpritesheet(Bitmap source, int targetSpriteSize)
+        {
+            // get number of rows from builtin image (premature optimization if it will be extended downwards for more character space),
+            // assuming individual sprites are square
+            var spriteRows = source.Height / (source.Width / Constants.SpritesPerRow);
+            
+            var scaledBitmap = new Bitmap(Constants.SpritesPerRow * targetSpriteSize, spriteRows * targetSpriteSize);
+
+            var sourceRect = new Rectangle(0, 0, source.Width, source.Height);
+            var destRect = new Rectangle(0, 0, scaledBitmap.Width, scaledBitmap.Height);
+
+            using (var graphics = System.Drawing.Graphics.FromImage(scaledBitmap))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                graphics.DrawImage(source, destRect, sourceRect, GraphicsUnit.Pixel);
+
+                return scaledBitmap;
+            }
+        }
     }
 }
