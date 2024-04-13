4beb76a936b597fa73c1150347ce04e7676710c6
Bn4 <bnfour@bnfour.net>
2024-04-13 20:08:42 +0700

    song info: version bumped to 1.0.0 in anticipation of upcoming release (probably tomorrow, but who knows?)

--- SongInfo/Properties/AssemblyInfo.cs
+++ SongInfo/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MuseDashMods.SongInfo;
 
-[assembly: MelonInfo(typeof(SongInfoMod), "Song info", "0.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonInfo(typeof(SongInfoMod), "Song info", "1.0.0", "bnfour", "https://github.com/bnfour/md-mods")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
