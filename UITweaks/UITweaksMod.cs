9ccf515ebc4eb38f28480985256a405fa64ce43e
Bn4 <bnfour@bnfour.net>
2024-12-07 00:28:59 +0700

    ui tweaks: an option to disable newly merged wider album titles

--- UITweaks/UITweaksMod.cs
+++ UITweaks/UITweaksMod.cs
@@ -5,16 +5,19 @@ namespace Bnfour.MuseDashMods.UITweaks;
 public class UITweaksMod : MelonMod
 {
     private MelonPreferences_Category _prefsCategory;
+    private MelonPreferences_Entry<bool> _widerTitlesEnabled;
 
-    internal bool WiderAlbumTitlesEnabled => true;
+    internal bool WiderAlbumTitlesEnabled => _widerTitlesEnabled.Value;
 
     public override void OnInitializeMelon()
     {
         base.OnInitializeMelon();
 
         _prefsCategory = MelonPreferences.CreateCategory("Bnfour_UITweaks");
+        _widerTitlesEnabled = _prefsCategory.CreateEntry("WiderAlbumTitles", true,
+            "Wider album titles", "Enables wider album titles on song selection screen.");
 
-        if (true)
+        if (!WiderAlbumTitlesEnabled)
         {
             LoggerInstance.Warning("No features of the mod enabled, might as well uninstall it.");
         }
