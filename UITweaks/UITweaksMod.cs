4ea45d089a9cb8f01316756002f440493e687736
Bn4 <bnfour@bnfour.net>
2024-12-07 00:42:12 +0700

    ui tweaks: options - to toggle the hp/fever sync altogether - to toggle the sync mode

--- UITweaks/UITweaksMod.cs
+++ UITweaks/UITweaksMod.cs
@@ -7,11 +7,13 @@ public class UITweaksMod : MelonMod
     private MelonPreferences_Category _prefsCategory;
     private MelonPreferences_Entry<bool> _widerTitlesEnabled;
     private MelonPreferences_Entry<bool> _cupImageSyncEnabled;
+    private MelonPreferences_Entry<bool> _hpFeverSyncEnabled;
+    private MelonPreferences_Entry<bool> _hpFeverSyncUseAltMode;
 
     internal bool WiderAlbumTitlesEnabled => _widerTitlesEnabled.Value;
     internal bool AchievementIconsSyncEnabled => _cupImageSyncEnabled.Value;
-    internal bool HpFeverFlowSyncEnabled => true;
-    internal bool HpFeverFlowSyncUseAltMode => false;
+    internal bool HpFeverFlowSyncEnabled => _hpFeverSyncEnabled.Value;
+    internal bool HpFeverFlowSyncUseAltMode => _hpFeverSyncUseAltMode.Value;
 
     public override void OnInitializeMelon()
     {
@@ -22,6 +24,10 @@ public class UITweaksMod : MelonMod
             "Wider album titles", "Enables wider album titles on song selection screen.");
         _cupImageSyncEnabled = _prefsCategory.CreateEntry("AchievementIconsSync", true,
             "Achievement icons sync", "Syncs the spinning cup images for song achievements.");
+        _hpFeverSyncEnabled = _prefsCategory.CreateEntry("SyncHpFeverAnim", true,
+            "HP and Fever bar animation sync", "Syncs the bubble animation for HP and Fever bars.");
+        _hpFeverSyncUseAltMode = _prefsCategory.CreateEntry("SyncHpFeverAnimAlt", false,
+            "Alternate HP-Fever sync", "Syncs HP bar to Fever bar.");
 
         if (!WiderAlbumTitlesEnabled && !AchievementIconsSyncEnabled && !HpFeverFlowSyncEnabled)
         {
