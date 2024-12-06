29eb4620c30a291f8f32fc16224d18485b024297
Bn4 <bnfour@bnfour.net>
2024-12-07 00:20:50 +0700

    ui tweaks: preparation for preferences to toggle specific mods from the bundle

--- UITweaks/UITweaksMod.cs
+++ UITweaks/UITweaksMod.cs
@@ -4,5 +4,17 @@ namespace Bnfour.MuseDashMods.UITweaks;
 
 public class UITweaksMod : MelonMod
 {
-    
+    private MelonPreferences_Category _prefsCategory;
+
+    public override void OnInitializeMelon()
+    {
+        base.OnInitializeMelon();
+
+        _prefsCategory = MelonPreferences.CreateCategory("Bnfour_UITweaks");
+
+        if (true)
+        {
+            LoggerInstance.Warning("No features of the mod enabled, might as well uninstall it.");
+        }
+    }
 }
