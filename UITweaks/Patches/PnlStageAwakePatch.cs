e1a5e1234ab609886b0ef379cc4646cb922c110e
Bn4 <bnfour@bnfour.net>
2024-12-27 22:59:43 +0700

    ui tweaks: fixed outdated comments for wider titles
    i really should have included this in v20

--- UITweaks/Patches/PnlStageAwakePatch.cs
+++ UITweaks/Patches/PnlStageAwakePatch.cs
@@ -8,7 +8,7 @@ namespace Bnfour.MuseDashMods.UITweaks.Patches;
 
 /// <summary>
 /// Makes the album title text on selection screen wide enough to fit all album
-/// title.
+/// titles.
 /// </summary>
 [HarmonyPatch(typeof(PnlStage), nameof(PnlStage.Awake))]
 public class PnlStageAwakePatch
@@ -24,14 +24,14 @@ public class PnlStageAwakePatch
         var objectTransform = __instance.m_AlbumTitleObj.GetComponent<RectTransform>();
         var textTransform = __instance.m_AlbumTitleTxt.GetComponent<RectTransform>();
 
-        // new width found empirically, fits all currently (4.10) available titles,
-        // the widest probably being maimai's pack full title
+        // new width found empirically, fits all currently (4.11) available titles,
+        // the widest being ba's pack full title
         objectTransform.sizeDelta = new Vector2(550, 60);
         textTransform.sizeDelta = new Vector2(550, 60);
 
-        // originally set to width - 86
         // makes the component aware of its new extra width
-        // we set it to cover slightly move because we don't expect any scroll to happen
+        // originally set to sizeDelta's width - 86
+        // set to cover slightly more because we don't expect any scrolling to happen
         __instance.m_AlbumTitleTxt.m_LongBound = 480;
     }
 }
