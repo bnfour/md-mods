37237d698833531f668ec0d51f88d5497bd652d6
Bn4 <bnfour@bnfour.net>
2025-01-04 14:46:58 +0700

    updated versions (+patch) for the mods i don't expect to change until a new release: so everything except song info (definitely will change) and scoreboard characters (depends on update having a new character)

--- TrueAbove1kRank/Properties/AssemblyInfo.cs
+++ TrueAbove1kRank/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MuseDashMods.TrueAbove1kRank;
 
-[assembly: MelonInfo(typeof(TrueAbove1kRankMod), "True rank for 999+", "2.0.0", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonInfo(typeof(TrueAbove1kRankMod), "True rank for 999+", "2.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
