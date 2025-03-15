30cf2fb1500bb415bcef74cb278fa970c5e12643
Bn4 <bnfour@bnfour.net>
2025-03-15 22:16:31 +0700

    ui tweaks: version bumped to 1.1.0 following addition of new tweaks
    i'm probably done with these until v23 release, but who knows ┐(￣ ヘ￣)┌

--- UITweaks/Properties/AssemblyInfo.cs
+++ UITweaks/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using MelonLoader;
 
 using Bnfour.MuseDashMods.UITweaks;
 
-[assembly: MelonInfo(typeof(UITweaksMod), "UI tweaks", "1.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonInfo(typeof(UITweaksMod), "UI tweaks", "1.1.0", "bnfour", "https://github.com/bnfour/md-mods")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
