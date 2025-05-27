7b6f6b9162f39f6287bb07f4e481f5d03e8ee8f2
Bn4 <bnfour@bnfour.net>
2025-05-27 23:57:15 +0700

    ui tweaks: fixed the title for the longest album being sligthly cut off on its ends by gradient/trasnparency masks
    this is committed as is as soon i got it working, now to clean up the "prototype" code somewhat
    
    basically, this shrinks the mask images in half so they no longer overlap with the "MD-level... ...Blu-ray" text. Technically, we can just not render them, but there are also some solid violet coloring on the edges of the entire area, into which the masks merge, and it looks scuffed without them, so they are merely shrunk instead. The right one is also moved to keep its position, because resizing keeps orininal top and left coordinates

--- UITweaks/Patches/PnlStageAwakePatch.cs
+++ UITweaks/Patches/PnlStageAwakePatch.cs
@@ -3,6 +3,7 @@ using MelonLoader;
 using UnityEngine;
 
 using Il2CppAssets.Scripts.UI.Panels;
+using UnityEngine.UI;
 
 namespace Bnfour.MuseDashMods.UITweaks.Patches;
 
@@ -33,5 +34,39 @@ public class PnlStageAwakePatch
         // originally set to sizeDelta's width - 86
         // set to cover slightly more because we don't expect any scrolling to happen
         __instance.m_AlbumTitleTxt.m_LongBound = 480;
+
+        var logger = Melon<UITweaksMod>.Logger;
+
+        logger.Msg($"root name {__instance.m_AlbumTitleObj.name}");
+
+        var maskL = __instance.m_AlbumTitleObj.transform.Find("DisplayArea/ImgMaskL");
+        var maskR = __instance.m_AlbumTitleObj.transform.Find("DisplayArea/ImgMaskR");
+
+        var cs = maskL.GetComponents<Component>();
+        foreach (var c in cs)
+        {
+            logger.Msg($"{c.name} -- {c.GetIl2CppType().Name}");
+        }
+
+        // maskL.GetComponent<Image>().color = Color.clear;
+        // maskR.GetComponent<Image>().color = Color.clear;
+
+        var rt = maskL.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2
+        {
+            x = rt.sizeDelta.x / 2,
+            y = rt.sizeDelta.y
+        };
+
+        var rtR = maskR.GetComponent<RectTransform>();
+        var originalRight = rtR.right;
+        rtR.sizeDelta = new Vector2
+        {
+            x = rtR.sizeDelta.x / 2,
+            y = rtR.sizeDelta.y
+        };
+        rtR.right = originalRight;
+
+        // logger.Msg($"l = null {maskL == null}, r = null {maskR == null}");
     }
 }
