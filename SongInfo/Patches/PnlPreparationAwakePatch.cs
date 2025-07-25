57d2b54b1e2b8e7d4fc32f993edaf20dfd22ab36
Bn4 <bnfour@bnfour.net>
2025-07-25 20:02:48 +0700

    song info: the data is now displayed as a single line, "01:34, 100 BPM" in order to not clip with the new vanilla UI;
    also switched away from global component search
    
    fun fact: string format is taken from my other mod of the same name; no "SV!" bit though

--- SongInfo/Patches/PnlPreparationAwakePatch.cs
+++ SongInfo/Patches/PnlPreparationAwakePatch.cs
@@ -1,33 +1,22 @@
 using HarmonyLib;
 using Il2Cpp;
-using MelonLoader;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Bnfour.MuseDashMods.SongInfo.Patches;
 
 /// <summary>
-/// Patch to modify the UI of preparation panel to include textfields
-/// for BPM and duration.
+/// Patch to modify the UI of preparation panel to include a textfield for BPM and duration.
 /// </summary>
 [HarmonyPatch(typeof(PnlPreparation), nameof(PnlPreparation.Awake))]
 public class PnlPreparationAwakePatch
 {
     private static void Postfix(PnlPreparation __instance)
     {
-        // clone the song designer string twice to display bpm and duration,
-        // place the clones on the right side of the screen
-
-        var logger = Melon<SongInfoMod>.Logger;
-
-        var bpmField = UnityEngine.Object.Instantiate(__instance.designerLongNameController,
+        // clone the song designer string to display bpm and duration,
+        // place the clone on the right side of the screen
+        var dataField = GameObject.Instantiate(__instance.designerLongNameController,
             __instance.designerLongNameController.transform.parent);
-        bpmField.name = Constants.BpmStringComponentName;
-        bpmField.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(1540, 0, 0);
-
-        var durationField = UnityEngine.Object.Instantiate(__instance.designerLongNameController,
-            __instance.designerLongNameController.transform.parent);
-        durationField.name = Constants.DurationStringComponentName;
-        durationField.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(1540, -45, 0);
+        dataField.name = Constants.CombinedStringComponentName;
+        dataField.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(1540, 0, 0);
     }
 }
