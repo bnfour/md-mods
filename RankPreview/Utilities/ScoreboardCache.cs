5253d042c739a52ae1aa5e42351cc49ef496c318
Bn4 <bnfour@bnfour.net>
2025-09-02 20:10:33 +0700

    rank preview: a "dictionary" implementation with limited capacity (so we don't store too much arrays of hundred ints); cache implemented to use that

--- RankPreview/Utilities/ScoreboardCache.cs
+++ RankPreview/Utilities/ScoreboardCache.cs
@@ -1,13 +1,17 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bnfour.MuseDashMods.RankPreview.Utilities;
 
 internal class ScoreboardCache
 {
+    // good enough to hold scoreboards for a few recently opened songs
+    private const int Capacity = 8;
+    private readonly LimitedCapacityDictionary<string, int[]> _backend = new(Capacity);
+
     internal void Store(string key, IEnumerable<int> value)
     {
-        // TODO
+        _backend[key] = value.ToArray();
     }
 
     // TODO do we want this (getting rank, formatting rank) as a single method?
@@ -15,6 +19,22 @@ internal class ScoreboardCache
     // -- handles missing entries and certainty (more on that later) internally
     internal string EstimateRank(string key, int currentScore)
     {
-        return "xddShrug";
+        if (_backend.ContainsKey(key))
+        {
+            var estimatedRank = _backend[key].TakeWhile(score => score >= currentScore).Count() + 1;
+
+            if (estimatedRank > 99)
+            {
+                return "#100+";
+            }
+
+            // TODO extra formatting?
+
+            return $"#{estimatedRank}";
+        }
+        else
+        {
+            return "¯\\_(ツ)_/¯";
+        }
     }
 }
