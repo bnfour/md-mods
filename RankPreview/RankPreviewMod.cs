9ce87a7f99c3bb02da60a7a9af3a355b56c2016d
Bn4 <bnfour@bnfour.net>
2025-09-02 15:33:43 +0700

    new project for a new mod!!
    this one is going to be a preview of how the just achieved would look on the scoreboard that was cached before starting a song, so i don't have to memorize and/or revisit the scoreboard after setting a highscore to confirm my position (and then maybe restart)
    
    i have been farming FCAPs on ~5* tracks and thought of this

---
+++ RankPreview/RankPreviewMod.cs
@@ -0,0 +1,10 @@
+using MelonLoader;
+
+namespace Bnfour.MuseDashMods.RankPreview;
+
+public class RankPreviewMod : MelonMod
+{
+    // TODO cache for scoreboards
+    // set on load
+    // displayed on victory screen
+}
