0d3538becb0a1ea3eb0464010b25045afb12fb97
Bn4 <bnfour@bnfour.net>
2025-10-22 15:09:23 +0700

    song info -- localization support: a simple class to provide localized strings
    a simple wrapper that provides strings for language ids already present in the game; it's so simple i won't even test it, though i'd like to test the code inside the Language struct, but i won't because it's not my code xddShrug

---
+++ SongInfo/Data/Locale.cs
@@ -0,0 +1,50 @@
+using MelonLoader;
+
+using Il2CppAssets.Scripts.Database;
+using Il2CppPeroPeroGames.GlobalDefines;
+
+namespace Bnfour.MuseDashMods.SongInfo.Data;
+
+/// <summary>
+/// Used to provide localized string to use in mod UI.
+/// </summary>
+internal static class Locale
+{
+    // see GlobalDefines.Language, order is
+    // 0: dummy "none" entry
+    // 1: English
+    // 2: Chinese Simplified
+    // 3: Chinese Traditional
+    // 4: Japanese
+    // 5: Korean
+    private static readonly string[] _lengthLocalizations = new[]
+    {
+        "[none]",
+        "[English]",
+        "[Chinese S]",
+        "[Chinese T]",
+        "[Japanese]",
+        "[Korean]"
+    };
+
+    /// <summary>
+    /// The term for song length (duration?) in the language the game currently uses.
+    /// </summary>
+    internal static string Length
+    {
+        get
+        {
+            // TODO can also check that Language.language_count matches our array's length
+
+            var index = Language.LanguageToIndex(DataHelper.userLanguage);
+            // LanguageToIndex returns -1 for unknown strings
+            if (index < 0)
+            {
+                Melon<SongInfoMod>.Logger.Warning($"Unknown language \"{DataHelper.userLanguage}\" (index {index}), defaulting to {_lengthLocalizations[0]}");
+                index = 0;
+            }
+
+            return _lengthLocalizations[index];
+        }
+    }
+}
