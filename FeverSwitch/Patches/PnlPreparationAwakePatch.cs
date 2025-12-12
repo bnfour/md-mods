655e9fd0709bb4af0005a637de79e8e7ec88d578
Bn4 <bnfour@bnfour.net>
2025-12-12 22:46:38 +0700

    fever switch: added a custom F keybind for the toggle, temporary tip image from existing assets to be replaced with fixed version
    default U still works (for now?) as i didn't bother to touch the relevant InputKeyBinding component
    
    i had been trying to figure out how rewired actions work for a few hours to no success; just couldn't find any maps for action 71, "ViewMode", which was originally (and still is) for the F3's menu new view mode; it's shared with the random toggle for some reason. If you know how to rebind rewired at runtime, hit me up
    
    anyways, the new component is a really simple one that only toggles the random (fever) switch on F key presses, no config at all

--- FeverSwitch/Patches/PnlPreparationAwakePatch.cs
+++ FeverSwitch/Patches/PnlPreparationAwakePatch.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 using Il2Cpp;
+using Il2CppUI.Controls.BtnTipStateControl;
+using Il2CppPeroTools2.Resources;
+
+using Bnfour.MuseDashMods.FeverSwitch.Components;
 
 namespace Bnfour.MuseDashMods.FeverSwitch.Patches;
 
 /// <summary>
-/// Slightly resizes the random toggle off image to be a 42×42 square to fit the auto icon.
+/// Updates the UI for the mod:<br/>
+/// - resizes the random toggle off image to be a 42×42 square to fit the auto icon<br/>
+/// - add a single copy of a custom keybinding component so that F for fever works
 /// </summary>
 [HarmonyPatch(typeof(PnlPreparation), nameof(PnlPreparation.Awake))]
 public class PnlPreparationAwakePatch
@@ -19,6 +25,19 @@ public class PnlPreparationAwakePatch
             ?.GetComponent<Image>()?.rectTransform is RectTransform rt)
         {
             rt.sizeDelta = new Vector2(42, 42);
+        }
+
+        if (__instance.transform.Find("RightRoot/Top/ImgRandomBg/ImgRandomFlagBg/KeyTip/ImgRandomPCTip")
+            ?.GetComponent<PnlLevelConfigRandomRoleKeyTipViewControl>() is PnlLevelConfigRandomRoleKeyTipViewControl control)
+        {
+            // TODO custom F sprite with PcButtonU proportions
+            control.m_ImgFront.sprite = ResourcesManager.instance.LoadFromName<Sprite>("ButtonF");
+        }
+        // add custom component to change the keybind, once or things will break
+        if (__instance.transform.Find("Panels/PnlRankLocalization/Pc/PnlRank")?.gameObject is GameObject go
+            && go.GetComponent<CustomFeverToggleKeybind>() == null)
+        {
+            go.AddComponent<CustomFeverToggleKeybind>();
         }
     }
 }
