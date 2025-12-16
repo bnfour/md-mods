b48b2d00e7ffdf7196cc6b5e7447394feb41f87e
Bn4 <bnfour@bnfour.net>
2025-12-16 23:59:59 +0700

    fever switch: reintroduced resizing of toggle's "off" sprite only (this time) after every update, now _probably_ works as originally intended
    at the cost of a very, ahem, "blunt", hierarchy traversal i'm not really happy with (".parent.parent.parent.parent", really?), but have no idea how to make better

--- FeverSwitch/Patches/PnlRankRefreshRandomLevelConfigUIPatch.cs
+++ FeverSwitch/Patches/PnlRankRefreshRandomLevelConfigUIPatch.cs
@@ -1,5 +1,7 @@
 using HarmonyLib;
 using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
 
 using Il2CppAssets.Scripts.Database;
 using Il2CppAssets.Scripts.UI.Panels;
@@ -35,8 +37,26 @@ public class PnlRankRefreshRandomLevelConfigUIPatch
             ? !DataHelper.isAutoFever : DataHelper.isAutoFever;
     }
 
-    internal static void Postfix()
+    internal static void Postfix(PnlRank __instance)
     {
+        // widen the image for the off toggle slightly from default 38×42 to 42×42 to better fit the custom sprites
+        if (!DataHelper.isUseRandomLevelConfig)
+        {
+            // TODO scuffed hierarchy traversal, could be better?
+            // from current PnlRank to (grand+)parent PnlPreparation,
+            // then by a known path to the image component
+
+            // first, ".../PnlPreparation/Panels/PnlRankLocalization/Pc/PnlRank" in reverse
+            if (__instance.transform.parent.parent.parent.parent
+            // then, descend into a different path
+                .Find("RightRoot/Top/ImgRandomBg/ImgRandomFlagBg/ImgRandomFlag")
+                ?.GetComponent<Image>()?.rectTransform is RectTransform rt)
+            {
+                rt.sizeDelta = new(42, 42);
+            }
+        }
+
+        // turn off the random mode if it was used to enable the now-fever toggle
         DataHelper.isUseRandomLevelConfig = false;
     }
 }
