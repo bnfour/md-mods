762d1bef70123c3935ae51d4552837040672a9a1
Bn4 <bnfour@bnfour.net>
2026-01-15 21:16:56 +0700

    provisionally bumped versions +patch for already released mods following the year bump
    fever switch is not affected, and is going to be shipped as 1.0.0

--- RankPreview/Properties/AssemblyInfo.cs
+++ RankPreview/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using MelonLoader;
 
 using Bnfour.MuseDashMods.RankPreview;
 
-[assembly: MelonInfo(typeof(RankPreviewMod), "Rank preview", "1.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonInfo(typeof(RankPreviewMod), "Rank preview", "1.0.2", "bnfour", "https://github.com/bnfour/md-mods")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
