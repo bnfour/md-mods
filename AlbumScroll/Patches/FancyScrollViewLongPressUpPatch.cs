ecbbbdca8a8cf98df829acbd49557e55c82fef9b
Bn4 <bnfour@bnfour.net>
2026-01-19 15:31:15 +0700

    album scroll -- major refactor: now tracks the shift keys state in the extra component attached to _the_ songs' scrollview, as opposed to the entire UI scene;
    the mod class now does nothing, ComponentVerifier removed
    
    added a custom component class that tracks l/r shift keypresses, attached to songs' scrollview at awake of the parent panel

--- AlbumScroll/Patches/FancyScrollViewLongPressUpPatch.cs
+++ AlbumScroll/Patches/FancyScrollViewLongPressUpPatch.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
-using MelonLoader;
 
 using Il2CppAssets.Scripts.PeroTools.Nice.Components;
 
+using Bnfour.MuseDashMods.AlbumScroll.Components;
 using Bnfour.MuseDashMods.AlbumScroll.Data;
 using Bnfour.MuseDashMods.AlbumScroll.Utilities;
 
@@ -13,26 +13,23 @@ namespace Bnfour.MuseDashMods.AlbumScroll.Patches;
 [HarmonyPatch]
 public class FancyScrollViewLongPressUpPatch
 {
-    // see https://harmony.pardeike.net/articles/patching-auxiliary.html#targetmethods
-    // and https://harmony.pardeike.net/articles/patching-injections.html#__originalmethod
-    // for details on patching multiple similar methods with a single patch
-    // it's pretty cool
-    
-    private static IEnumerable<MethodBase> TargetMethods()
+    internal static IEnumerable<MethodBase> TargetMethods()
     {
         yield return AccessTools.Method(typeof(FancyScrollView), nameof(FancyScrollView.LongPressPreviousUp));
         yield return AccessTools.Method(typeof(FancyScrollView), nameof(FancyScrollView.LongPressNextUp));
     }
 
-    [HarmonyPostfix]
-    private static void Postfix(FancyScrollView __instance, MethodBase __originalMethod)
+    internal static void Postfix(FancyScrollView __instance, MethodBase __originalMethod)
     {
-        if (Melon<AlbumScrollMod>.Instance.ShiftDown
-            && ComponentVerifier.IsActiveSongScrollView(__instance))
+        if (__instance.gameObject.GetComponent<ShiftStateTracker>() is ShiftStateTracker stateTracker
+            && stateTracker.ShiftDown)
         {
-            var dir = __originalMethod.Name.Contains("Next")
-                ? Direction.Forward
-                : Direction.Backward;
+            var dir = __originalMethod.Name switch
+            {
+                nameof(FancyScrollView.LongPressNextUp) => Direction.Forward,
+                nameof(FancyScrollView.LongPressPreviousUp) => Direction.Backward,
+                _ => throw new System.ApplicationException("Unknown FSV directional press method.")
+            };
 
             AlbumScroller.ScrollToDifferentAlbum(__instance, dir);
         }
