24c6962b571a43d69a55cf9e484b3e4cf9f64ff0
Bn4 <bnfour@bnfour.net>
2026-03-13 02:30:22 +0700

    fever switch -- key tip state on open: fixed all the wip points from the previous commit

--- FeverSwitch/Patches/PnlLevelConfigRandomRoleKeyTipViewControlOnEnablePatch.cs
+++ FeverSwitch/Patches/PnlLevelConfigRandomRoleKeyTipViewControlOnEnablePatch.cs
@@ -1,24 +1,27 @@
 using HarmonyLib;
+
 using Il2CppAssets.Scripts.Database;
 using Il2CppUI.Controls.BtnTipStateControl;
-using MelonLoader;
+
+using Bnfour.MuseDashMods.FeverSwitch.Utilities;
 
 namespace Bnfour.MuseDashMods.FeverSwitch.Patches;
 
 /// <summary>
-/// 
+/// Sets the "random mode" flag to match current fever state for visual consistency
+/// for key tip component init (its color depends on the value).
+/// See #35.
 /// </summary>
 [HarmonyPatch(typeof(PnlLevelConfigRandomRoleKeyTipViewControl), nameof(PnlLevelConfigRandomRoleKeyTipViewControl.OnEnable))]
-public class XddPatch
+public class PnlLevelConfigRandomRoleKeyTipViewControlOnEnablePatch
 {
     internal static void Prefix(out bool __state)
     {
-        Melon<FeverSwitchMod>.Logger.Msg("got em");
         // save the current state, whatever it is (false, probably; not that we care)
+        // to restore after we're done enabling this
         __state = DataHelper.isUseRandomLevelConfig;
         // set to match the fever state for the duration of the patched method
-        DataHelper.isUseRandomLevelConfig = Melon<FeverSwitchMod>.Instance.IsAutoDefault
-            ? !DataHelper.isAutoFever : DataHelper.isAutoFever;
+        DataHelper.isUseRandomLevelConfig = FlagOverrideValueProvider.Override;
     }
 
     internal static void Postfix(bool __state)
