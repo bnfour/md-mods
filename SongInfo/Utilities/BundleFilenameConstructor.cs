b9e20c84d912f08384b886ea559a26ff9d9fdd0d
Bn4 <bnfour@bnfour.net>
2026-04-29 19:18:30 +0700

    song info: actually understood the bundle file naming scheme, greatly simplified BundleFilenameConstructor as a result (not sure it even needs to be a class after that)
    in vanilla, _all_ instances of _music are stripped from the MusicInfo.music id to form the bundle name, even if they are from song's title (like "39 music") -- no idea why i didn't think of it before xddShrug

--- SongInfo/Utilities/BundleFilenameConstructor.cs
+++ SongInfo/Utilities/BundleFilenameConstructor.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-
 using Il2CppAssets.Scripts.Database;
 
 namespace Bnfour.MuseDashMods.SongInfo.Utilities;
@@ -10,25 +7,13 @@ namespace Bnfour.MuseDashMods.SongInfo.Utilities;
 /// </summary>
 internal static class BundleFilenameConstructor
 {
-    private static readonly Regex FileNameCleaningRegex = new(@"_music$");
-
-    // "music" in song name leads to non-standard naming for some reason
-    private static readonly Dictionary<string, string> Exceptions = new()
-    {
-        ["39_music_music"] = "music_39_assets_all.bundle",
-        ["computer_music_girl_music"] = "music_computer_girl_assets_all.bundle",
-        ["enka_dance_music_music"] = "music_enka_dance_assets_all.bundle",
-        ["dont_fight_the_music_music"] = "music_dont_fight_the_assets_all.bundle"
-    };
-
     internal static string IdToFilename(MusicInfo info)
     {
-        if (Exceptions.ContainsKey(info?.music))
-        {
-            return Exceptions[info?.music];
-        }
         // MusicInfo has music as "{id}_music", e.g. "inferno_city_music"
         // its data (not only music) is stored in a bundle named "music_{id}_assets_all.bundle", e.g. "music_inferno_city_assets_all.bundle"
-        return $"music_{FileNameCleaningRegex.Replace(info.music, string.Empty, 1)}_assets_all.bundle";
+
+        // but for some reason _all_ instances of word "music" are stripped -- even if they are technically part of the song title;
+        // so "computer_music_girl_music" is stored in "music_computer_girl_assets_all.bundle"
+        return $"music_{info?.music.Replace("_music", string.Empty)}_assets_all.bundle";
     }
 }
