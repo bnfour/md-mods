b0c4de50e9f670bff120fe59d22f45b5ed57a930
Bn4 <bnfour@bnfour.net>
2026-05-15 14:29:13 +0700

    color score status: data enum for distinct states, a mod property to hold it and ensure that it can only be downgraded in-game (AP -> FC or YT, FC -> YT only, YT -> no changes allowed)
    (YT is short for "you tried", that one does not have a established name so i'm just going to call it whatever)

--- ColorScoreStatus/ColorScoreStatusMod.cs
+++ ColorScoreStatus/ColorScoreStatusMod.cs
@@ -1,8 +1,32 @@
+using System;
 using MelonLoader;
+
+using Bnfour.MuseDashMods.ColorScoreStatus.Data;
 
 namespace Bnfour.MuseDashMods.ColorScoreStatus;
 
 public class ColorScoreStatusMod : MelonMod
 {
-    // TODO store refs to active score, current combo state and whatnot
+    internal ComboStatus Status
+    {
+        get => _status;
+        set
+        {
+            // only status downgrades are allowed in-game,
+            // reset to default AP is done separately as a part of ResetState()
+            if ((int)value < (int)_status)
+            {
+                throw new InvalidOperationException("Unable to upgrade the combo status.");
+            }
+
+            _status = value;
+        }
+    }
+
+    private ComboStatus _status;
+
+    internal void ResetState()
+    {
+        _status = ComboStatus.AllPerfect;
+    }
 }
