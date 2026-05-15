8e773789111951741f2b3d9d7f657c8a9b3051ad
Bn4 <bnfour@bnfour.net>
2026-05-16 02:08:08 +0700

    color score status: fixed yume's skill not downgrading display from AP to FC upon triggering
    i don't really care about characters other than yume and the big three, so there might be similar issues with them; same for the elfins that mess with the judgements instead of adding to the score
    
    open an issue or whatever xddShrug

---
+++ ColorScoreStatus/Patches/SkillManagerYumeSkillPatch.cs
@@ -0,0 +1,24 @@
+using HarmonyLib;
+using Il2CppAssets.Scripts.GameCore.Managers;
+using MelonLoader;
+
+using Bnfour.MuseDashMods.ColorScoreStatus.Data;
+
+namespace Bnfour.MuseDashMods.ColorScoreStatus.Patches;
+
+/// <summary>
+/// Downgrades the AP to FC on Yume's skill trigger, which is not reported as a
+/// miss in the general judgement event patch.
+/// </summary>
+[HarmonyPatch(typeof(SkillManager), nameof(SkillManager.YumeSkill))]
+public class SkillManagerYumeSkillPatch
+{
+    internal static void Postfix()
+    {
+        var mod = Melon<ColorScoreStatusMod>.Instance;
+        if (mod.Status == ComboStatus.AllPerfect)
+        {
+            mod.Status = ComboStatus.FullCombo;
+        }
+    }
+}
