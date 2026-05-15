d178b2c412fbf7657f3ef78e9dfc259610071672
Bn4 <bnfour@bnfour.net>
2026-05-15 14:16:19 +0700

    a new project for another mod!
    this is a smaller one i had the idea for about a year ago, it simply colors the score based on the current status, as an alternative for vanilla ap indicator; also will point out non-AP FCs unlike vanilla (maybe it does, i never actually used it)
    
    i don't _need_ the feature, and consider this mod more of a practice in reversing and architecture design; still, should in theory be useful

---
+++ ColorScoreStatus/ColorScoreStatusMod.cs
@@ -0,0 +1,8 @@
+using MelonLoader;
+
+namespace Bnfour.MuseDashMods.ColorScoreStatus;
+
+public class ColorScoreStatusMod : MelonMod
+{
+    // TODO store refs to active score, current combo state and whatnot
+}
