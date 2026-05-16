cb8fa357aaa624fbe13ff0638baee934e659ce9d
Bn4 <bnfour@bnfour.net>
2026-05-17 01:05:49 +0700

    color score status: test code for gc coloring and its results as comments
    while the texture gradient are simplest thing ever (two colors), i will probably ship them as images to not have a dependency on a graphic library just for that

--- ColorScoreStatus/Utilities/ColorSetting/GCScoreColorer.cs
+++ ColorScoreStatus/Utilities/ColorSetting/GCScoreColorer.cs
@@ -20,6 +20,32 @@ internal class GCScoreColorer(GameObject gc) : ScoreColorerBase(gc), IScoreColor
         // - colors set as material properties (glow, outline)
         // and actually implement the coloring
         var score = _reference.GetComponent<TextMeshProUGUI>();
-        Melon<ColorScoreStatusMod>.Logger.Msg($"just pretend the status was visibly set to {status} LULE");
+        var logger = Melon<ColorScoreStatusMod>.Logger;
+        logger.Msg($"just pretend the status was visibly set to {status} LULE");
+
+        logger.Msg($"text color: {score.color.r}, {score.color.g}, {score.color.b}");
+        // 1 1 1 — just white for the texture i guess
+
+        logger.Msg($"text gradient: {score.colorGradient}");
+        // some struct probably ignored in favor of the texture
+
+        logger.Msg($"text outline: {score.outlineColor.r}, {score.outlineColor.g}, {score.outlineColor.b} (width {score.outlineWidth})");
+        // #ffffff, but width 0, not shown?
+
+        logger.Msg($"text face texture {score.font?.material.GetTexture("_FaceTex")?.name}");
+        // gradient as a 32×32 texture, ScoreTexGC
+
+        if (score.fontMaterial.HasProperty("_GlowColor"))
+        {
+            var glow = score.fontMaterial.GetColor("_GlowColor");
+            logger.Msg($"glow color: {glow.r}, {glow.g}, {glow.b}");
+            // some light blue about 75% on the gradient top to down
+        }
+        if (score.fontMaterial.HasProperty("_OutlineColor"))
+        {
+            var outline = score.fontMaterial.GetColor("_OutlineColor");
+            logger.Msg($"outline color: {outline.r}, {outline.g}, {outline.b}");
+            // 1 1 1, matches the screenshot
+        }
     }
 }
