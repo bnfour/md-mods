c828b80664ed55152605cbb431c8e8bf6b1ae979
Bn4 <bnfour@bnfour.net>
2026-05-17 01:20:40 +0700

    color score status: entry for the groove coaster's glow color
    and by "precalculated" i mean "i had sticked the color picker to (0, 23) of the gradient textures"; the actual textures coming up a bit later, the palette class only provides colors, not textures

--- ColorScoreStatus/Data/Palette.cs
+++ ColorScoreStatus/Data/Palette.cs
@@ -6,27 +6,31 @@ internal record Palette
 {
     internal Color Main { get; init; }
     internal Color Outline { get; init; }
-
-    // TODO GC props as needed
+    // roughly defined as 75% between the main color and the lightest color from the reference S sprite,
+    // precalculated
+    internal Color GCGlow { get; init; }
 
     // TODO consider darkening the outlines
 
     // based on gold S
     internal static Palette AllPerfect => new()
     {
-        Main = new Color(1f, 0.941f, 0f),
-        Outline = new Color(1f, 0.8f, 0f)
+        Main = new(1f, 0.941f, 0f),
+        Outline = new(1f, 0.8f, 0f),
+        GCGlow = new(1f, 0.984f, 0.643f)
     };
     // based on silver S
     internal static Palette FullCombo => new()
     {
-        Main = new Color(0.8f, 0.941f, 0.996f),
-        Outline = new Color(0.608f, 0.667f, 0.925f)
+        Main = new(0.8f, 0.941f, 0.996f),
+        Outline = new(0.608f, 0.667f, 0.925f),
+        GCGlow = new(0.945f, 0.984f, 1f)
     };
     // based on red S
     internal static Palette YouTried => new()
     {
-        Main = new Color(1f, 0f, 0.529f),
-        Outline = new Color(0.765f, 0.11f, 0.541f)
+        Main = new(1f, 0f, 0.529f),
+        Outline = new(0.765f, 0.11f, 0.541f),
+        GCGlow = new(1f, 0.514f, 0.863f)
     };
 }
