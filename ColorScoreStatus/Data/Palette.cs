beb47ffa17854e5885163ca1d920fd02cc66594f
Bn4 <bnfour@bnfour.net>
2026-05-24 16:30:56 +0700

    repo -- random refactoring: records
    now i that i've read the docs again, i know that primary constructors for records create get-init properties for their parameters automatically -- and so i can shorten some code

--- ColorScoreStatus/Data/Palette.cs
+++ ColorScoreStatus/Data/Palette.cs
@@ -2,34 +2,38 @@ using UnityEngine;
 
 namespace Bnfour.MuseDashMods.ColorScoreStatus.Data;
 
-internal record Palette
+/// <summary>
+/// Holds colors used to change the score text color.
+/// </summary>
+/// <param name="Main">Main score color. Taken straight from the reference grade sprite.</param>
+/// <param name="Outline">Outline color (when applicable). Darkest color from the reference sprite,
+/// has its HSV value reduced by additional 33 (in 0–100 range).</param>
+/// <param name="GCGlow">Glow color for Groove Coaster text effects.
+/// 3/4 on the gradient from main color to reference sprite's lightest color.</param>
+internal record Palette(Color Main, Color Outline, Color GCGlow)
 {
-    // main color of the grade sprite
-    internal Color Main { get; init; }
-    // darkest color from the grade sprite -> HSV value reduced by 33 flat
-    internal Color Outline { get; init; }
-    // roughly defined as 75% between the main color and the lightest color from the reference S sprite
-    internal Color GCGlow { get; init; }
-
-    // based on gold S
-    internal static Palette AllPerfect => new()
-    {
-        Main = new(1f, 0.941f, 0f),
-        Outline = new(0.67f, 0.536f, 0f),
-        GCGlow = new(1f, 0.984f, 0.643f)
-    };
-    // based on silver S
-    internal static Palette FullCombo => new()
-    {
-        Main = new(0.8f, 0.941f, 0.996f),
-        Outline = new(0.391f, 0.429f, 0.595f),
-        GCGlow = new(0.945f, 0.984f, 1f)
-    };
-    // based on red S
-    internal static Palette YouTried => new()
-    {
-        Main = new(1f, 0f, 0.529f),
-        Outline = new(0.435f, 0.062f, 0.308f),
-        GCGlow = new(1f, 0.514f, 0.863f)
-    };
+    /// <summary>
+    /// Based on golden S sprite. Used for AP scores.
+    /// </summary>
+    internal static Palette AllPerfect => new(
+        new(1f, 0.941f, 0f),
+        new(0.67f, 0.536f, 0f),
+        new(1f, 0.984f, 0.643f)
+    );
+    /// <summary>
+    /// Based on silver S sprite. Used for non-AP FC scores.
+    /// </summary>
+    internal static Palette FullCombo => new(
+        new(0.8f, 0.941f, 0.996f),
+        new(0.391f, 0.429f, 0.595f),
+        new(0.945f, 0.984f, 1f)
+    );
+    /// <summary>
+    /// Based on red S sprite. Used for non-FC scores.
+    /// </summary>
+    internal static Palette YouTried => new(
+        new(1f, 0f, 0.529f),
+        new(0.435f, 0.062f, 0.308f),
+        new(1f, 0.514f, 0.863f)
+    );
 }
