474f2375bea6e606a6d6d8619faad8b886a2c606
Bn4 <bnfour@bnfour.net>
2026-05-29 16:58:25 +0700

    song info: version bumped +patch following data update and a small bundle naming thingy change
    i've decided, data updates are +patch, +minor is reserved for something more breaking now

--- SongInfo/Properties/AssemblyInfo.cs
+++ SongInfo/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using MelonLoader;
 
 using Bnfour.MuseDashMods.SongInfo;
 
-[assembly: MelonInfo(typeof(SongInfoMod), "Song info", "2.0.0", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonInfo(typeof(SongInfoMod), "Song info", "2.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
