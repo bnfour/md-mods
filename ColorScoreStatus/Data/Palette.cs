d0d69c31d09539c24bf5889f919916b277b990fb
Bn4 <bnfour@bnfour.net>
2026-06-29 03:06:25 +0700

    color score status: GCGlow is now calculated at runtime from two set colors
    it's on the gradient we're have to create for the texture
    
    it's _technically_ a different color now, but i doubt anyone'll go "what? the golden glow color's blue is now 0.659 instead of 0.643?? literally unplayable"
    
    i'm not going to update the screenshots unless i get very very very bored

--- ColorScoreStatus/Data/Palette.cs
+++ ColorScoreStatus/Data/Palette.cs
@@ -8,9 +8,9 @@ namespace Bnfour.MuseDashMods.ColorScoreStatus.Data;
 /// <param name="Main">Main score color. Taken straight from the reference grade sprite.</param>
 /// <param name="Outline">Outline color (when applicable). Darkest color from the reference sprite,
 /// has its HSV value reduced by additional 33 (in 0–100 range).</param>
-/// <param name="GCGlow">Glow color for Groove Coaster text effects.
-/// 3/4 on the gradient from main color to reference sprite's lightest color.</param>
-internal record Palette(Color Main, Color Outline, Color GCGlow)
+/// <param name="Light">Light score color, used in Groove Coaster skin gradients.
+/// Lightest color from the reference sprite.</param>
+internal record Palette(Color Main, Color Outline, Color Light)
 {
     /// <summary>
     /// Based on golden S sprite. Used for AP scores.
@@ -18,7 +18,7 @@ internal record Palette(Color Main, Color Outline, Color GCGlow)
     internal static Palette AllPerfect => new(
         new(1f, 0.941f, 0f),
         new(0.67f, 0.536f, 0f),
-        new(1f, 0.984f, 0.643f)
+        new(1f, 1f, 0.878f)
     );
     /// <summary>
     /// Based on silver S sprite. Used for non-AP FC scores.
@@ -26,7 +26,7 @@ internal record Palette(Color Main, Color Outline, Color GCGlow)
     internal static Palette FullCombo => new(
         new(0.8f, 0.941f, 0.996f),
         new(0.391f, 0.429f, 0.595f),
-        new(0.945f, 0.984f, 1f)
+        Color.white
     );
     /// <summary>
     /// Based on red S sprite. Used for non-FC scores.
@@ -34,8 +34,13 @@ internal record Palette(Color Main, Color Outline, Color GCGlow)
     internal static Palette YouTried => new(
         new(1f, 0f, 0.529f),
         new(0.435f, 0.062f, 0.308f),
-        new(1f, 0.514f, 0.863f)
+        new(1f, 0.702f, 0.984f)
     );
+
+    /// <summary>
+    /// Glow color for Groove Coaster text effects.
+    /// </summary>
+    internal Color GCGlow => Color.Lerp(Main, Light, 0.75f);
 
     /// <summary>
     /// Palette associated with a given combo status.
