2be33434772f5b68353f8b2b437113fd48ef57d6
Bn4 <bnfour@bnfour.net>
2026-07-30 04:47:20 +0700

    softer background dim: version promoted to 1.0.0
    it spent like a month "in testing" (me having Experimental.dll installed while playing normally), on a smaller side, and even has some tests — it should be ready for the Friday release Clueless

--- SofterBackgroundDim/Properties/AssemblyInfo.cs
+++ SofterBackgroundDim/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using MelonLoader;
 
 using Bnfour.MuseDashMods.SofterBackgroundDim;
 
-[assembly: MelonInfo(typeof(SofterBackgroundDimMod), "Softer background dim", "0.0.1", "bnfour", "https://github.com/bnfour/md-mods")]
+[assembly: MelonInfo(typeof(SofterBackgroundDimMod), "Softer background dim", "1.0.0", "bnfour", "https://github.com/bnfour/md-mods")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonColor(255, 202, 80, 16)]
 [assembly: MelonAuthorColor(255, 128, 128, 128)]
