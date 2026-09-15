ed4f3fe6c5ad5bf718afccc83af5b4325798d134
Bn4 <bnfour@bnfour.net>
2026-09-15 22:35:18 +0700

    ui tweaks: actually fix the album title for groove coaster collab

--- UITweaks/Patches/GrooveAlbumTitleFix.cs
+++ UITweaks/Patches/GrooveAlbumTitleFix.cs
@@ -8,8 +8,18 @@ namespace Bnfour.MuseDashMods.UITweaks.Patches;
 [HarmonyPatch(typeof(DBConfigLocalAlbums), nameof(DBConfigLocalAlbums.GetLocalTitleByIndex))]
 public class GrooveAlbumTitleFix
 {
-    internal static void Postfix(int index, ref string __result)
+    internal static void Postfix(ref string __result)
     {
-        Melon<UITweaksMod>.Logger.Msg($"Album name for id {index} is {__result}");
+        // TODO do nothing if turned off via config
+
+        // index is not static, as new albums are added to the start of the list,
+        // so we can't use a constant number to refer to the album
+        // (72 for 6.6.0, would likely increase by 1 for each new album added)
+        // note: the extra space; fullwidth exclamation mark
+        //                   V        V
+        if (__result == "Let' s GROOVE！")
+        {
+            __result = "Let's GROOVE!";
+        }
     }
 }
